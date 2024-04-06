6ea5ecba057a1d1fd4883233f53c7f7de64d495d
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-04-06 15:13:26 +0100

    completed product and category handler

---
+++ sportsstore/models/repository.go
@@ -0,0 +1,11 @@
+package models
+
+type Repository interface{
+	GetProduct(id int)Product
+	GetProducts()[]Product
+	GetProductPage(page, pageSize int) (products []Product, totalAvailable int)
+	GetCategories()[]Category
+	GetProductPageCategory(categroyId int, page, pageSize int)(products []Product, totalAvailable int)
+	Seed()
+}
+
